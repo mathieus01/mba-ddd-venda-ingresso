1cff8362a09dbe3d38bbea6b060fd7e05d943d29
Matheus N <mathieusnunes@gmail.com>
2024-04-17 10:49:28 -0300

    feat: created events and partner aggregate and its business logic

---
+++ src/@core/events/domain/entities/__tests__/partner.entity.spec.ts
@@ -0,0 +1,16 @@
+import { Partner } from '../partner.entity';
+
+test('deve criar um evento', () => {
+  const partner = Partner.create({
+    name: 'Parceiro 1',
+  });
+
+  const event = partner.initEvent({
+    name: 'Evento 1',
+    description: 'Descrição do evento 1',
+    date: new Date(),
+  });
+
+  expect(event).toBeDefined();
+  expect(partner).toBeDefined();
+});
